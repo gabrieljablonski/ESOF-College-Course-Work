20cbb12ad8ac61d59a1983cb745f4af271452776
krawby <gabriel659@hotmail.com>
2017-04-20 13:48:02 -0300

    Update PasswordDetect.py

--- PasswordDetect.py
+++ PasswordDetect.py
@@ -1,3 +1,7 @@
+#! python3
+# Defines a function that checks an user input for a password that is 9 characters or longer and if it has at least one
+# lowercase letter, one uppercase letter and a digit.
+
 import re
 
 def StrongPassword (password):
@@ -9,16 +13,16 @@ def StrongPassword (password):
     passwordRegexUpper=re.compile(r'[A-Z]')
     passwordRegexDigit=re.compile(r'[0-9]')
     
-    moForPassword=passwordRegexLower.search(password)
-    if moForPassword==None:
+    moForPassword=passwordRegexLower.search(password) #.findall() not necessary
+    if moForPassword==None: #check for at least one character in [a-z]
         return False
     
     moForPassword=passwordRegexDigit.search(password)
-    if moForPassword==None:
+    if moForPassword==None: #check for at least one character in [0-9]
         return False
     
     moForPassword=passwordRegexUpper.search(password)
-    if moForPassword==None:
+    if moForPassword==None: #check for at least one character in [A-Z]
         return False
     
     return True
